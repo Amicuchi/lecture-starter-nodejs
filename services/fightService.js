83d390ee03ea2f76ae76eab5df19a92cc4d07eb3
Amicuchi <AndersonAmicuchi@gmail.com>
2025-05-09 22:23:29 -0300

    feat: begins implementation of the fight service with history and creation of fights

--- services/fightService.js
+++ services/fightService.js
@@ -1,7 +1,54 @@
 import { fightRepository } from "../repositories/fightRepository.js";
+import { fighterRepository } from "../repositories/fighterRepository.js";
 
 class FightersService {
-  // OPTIONAL TODO: Implement methods to work with fights
+  createFight(fighter1Id, fighter2Id) {
+    const fighter1 = fighterRepository.getOne({ id: fighter1Id });
+    const fighter2 = fighterRepository.getOne({ id: fighter2Id });
+
+    if (!fighter1 || !fighter2) {
+      throw new Error("One or both fighters were not found.");
+    }
+
+    // --- Start of Fight Logic ---
+    let fightLog = [];
+    let attacker = fighter1;
+    let defender = fighter2;
+    let winner = null;
+
+    // Simple turn-based simulation
+    for (let i = 0; i < 10; i++) { // Maximum of 10 turns, for example
+      const damage = Math.max(1, attacker.power - defender.defense);
+      defender.health -= damage;
+      fightLog.push(`${attacker.name} attacks ${defender.name} causing ${damage} damage. ${defender.name} has ${defender.health} health left.`);
+
+      if (defender.health <= 0) {
+        winner = attacker;
+        fightLog.push(`${defender.name} was defeated! ${attacker.name} is the winner!`);
+        break;
+      }
+      // Swap attacker and defender
+      [attacker, defender] = [defender, attacker];
+    }
+
+    if (!winner) {
+      fightLog.push("The fight ended in a draw after many turns!");
+    }
+    // --- End of Fight Logic ---
+
+    const fightResult = {
+      fighter1: fighter1Id,
+      fighter2: fighter2Id,
+      log: fightLog,
+      winner: winner ? winner.id : null,
+    };
+
+    return fightRepository.create(fightResult);
+  }
+
+  getAllFights() {
+    return fightRepository.getAll();
+  }
 }
 
 const fightersService = new FightersService();
